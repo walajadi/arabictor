a52ff5bf18d987daa656aa3c582ed0fc5e4bf21f
Oussaid Slimane <walajadi@yahoo.fr>
2015-06-02 22:57:51 +0200

    test

--- translator.py
+++ translator.py
@@ -6,5 +6,12 @@ Created on 2 juin 2015
 
 @author: slim
 '''
-print 'hello world'
+import urllib2
 
+
+print 'hello world'
+the_url = 'http://www.voidspace.org.uk'
+req = urllib2.Request(the_url)
+handle = urllib2.urlopen(req)
+the_page = handle.read()
+print the_page
